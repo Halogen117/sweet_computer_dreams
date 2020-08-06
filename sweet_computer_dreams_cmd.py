c2b6d74226298cafc66ad209a6a9408655ca391c
Russel Tan Jun Hong <47584079+Halogen117@users.noreply.github.com>
2020-08-06 15:33:32 +0800

    Update 2.1
    It has been a while since I updated this branch! Been busy with certifications and volunteering! This update introduced new bug fixes on the program, alongside the use of youtube-dl to do the work of filtering the youtube playlist instead of BeautifulSoup. Might have been due to change of Youtube data structuring

--- sweet_computer_dreams_cmd.py
+++ sweet_computer_dreams_cmd.py
@@ -11,32 +11,38 @@ import argparse
 import tkinter
 # Add argument to play youtube playlist!
 import pafy
+import os
+#os.chdir('C:\Program Files\VideoLAN\VLC')
 import vlc
 from bs4 import BeautifulSoup
 import requests
 import random
+
+
+# Function that scraps website to get Youtube playlist links
+# Not working atm
 def getPlaylistLinks(url):
     dict_append = {}
     count = 0
-    source_code = requests.get(url).text
+    import subprocess
+    import json
+    #command =  'youtube-dl --dump-json https://www.youtube.com/playlist?list=PLy8QXiv7X7KGWy6raCmBTlRC8Vz7V2I2p > "'+os.getcwd()+'\\json.json"'
+    command = 'youtube-dl -j --flat-playlist https://www.youtube.com/playlist?list=PLy8QXiv7X7KFygflzVEYfLB0_p1IhX9S7 > "'+os.getcwd()+'\\json.json"'
+    completed_process = subprocess.run(command,shell=True)
 
-    soup = BeautifulSoup(source_code, 'lxml')
-    domain = 'https://www.youtube.com'
+    f = open(os.getcwd()+"\\json.json", "r")
+    for x in f:
+        load_json = json.loads(x)
+        dict_append[count] = "https://www.youtube.com/watch?v="+load_json["id"]
+        count +=  1
+    f.close()
+    os.remove(os.getcwd()+"\\json.json")
+    if len(dict_append) == 0:
+        return {1:'https://www.youtube.com/watch?v=xf9ZtTC2aHw'}
+    else:
+        return dict_append
 
-    for i, tr in enumerate(soup.select('tr.pl-video')):
-        #print('{}. {}'.format(i + 1, tr['data-title']))
-        #print(domain + tr.a['href'])
-        #print('-' * 80)
-        dict_append[count] = [domain + tr.a['href'], tr['data-title']]
-        count += 1
-        
-    return dict_append
-
- 
-
-
-
-
+# Function that convert all time to seconds
 def calculate_to_seconds(args_days,args_hours,args_minutes,args_seconds):
     args_days = args_days or 0
     args_hours = args_hours or 0
@@ -44,14 +50,14 @@ def calculate_to_seconds(args_days,args_hours,args_minutes,args_seconds):
     args_seconds = args_seconds or 0
     return (args_days*3600*24) + (args_hours * 60 + args_minutes) * 60 + args_seconds
 
-
+# Function to enhance argument function
 def check_time_no_args():
     time_output = input("How long do you want to run the program before allowing the compute to sleep? (seconds)")
     try:
         return int(time_output)
     except ValueError:
         return False
-
+# Function to find Existance of shutdown file
 def check_shutdown_file():
     print("Checking shutdown program!")
     if os.path.isfile('C:\\Windows\\System32\\shutdown.exe'):
@@ -59,7 +65,7 @@ def check_shutdown_file():
     else:
         return False
 
-
+# Function to check if the function is running in an administrator mode
 def test_admin():
     try:
         is_admin = os.getuid() == 0
@@ -67,7 +73,6 @@ def test_admin():
         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
 
     return is_admin
-
 
 
 def main():
@@ -86,13 +91,21 @@ def main():
                     help='Enable this option to fully shutdown the computer along with all applications as well',default=None)
 
     parser.add_argument('-v', action='store',dest='playlist_url', 
-                    help='Change this argument for you to play a differetn youtube playlist',default='https://www.youtube.com/playlist?list=PLy8QXiv7X7KFygflzVEYfLB0_p1IhX9S7')   
+                    help='Change this argument for you to play a differetn youtube playlist',default='https://www.youtube.com/playlist?list=PLy8QXiv7X7KFygflzVEYfLB0_p1IhX9S7')
+
+    parser.add_argument('--p', action='store_true',dest='player_off',
+                    help='Set this option to turn on the video player with the vlc option')   
+
+    parser.add_argument('--p-c', action='store_false',dest='player_off',
+                    help='Set this option to turn off the video player with the vlc option')   
     args = parser.parse_args()
     args_seconds = args.second_timer
     args_minutes = args.minute_timer
     args_hours = args.hour_timer
     args_days = args.day_timer
     args_playlist_url = args.playlist_url
+    args_player_off = args.player_off
+
     if check_shutdown_file is False:
         print("No shutdown file in existance! Exiting Program!")
         exit()
@@ -107,64 +120,67 @@ def main():
         time_to_pause = calculate_to_seconds(args_days,args_hours,args_minutes,args_seconds)
     
     end_time = start_time + datetime.timedelta(seconds=time_to_pause)
-    
 
-    # Get the dictionary of the playlist
-    playlist_dict = getPlaylistLinks(args_playlist_url)
+    if args_player_off is True:
+        # Get the dictionary of the playlist
+        playlist_dict = getPlaylistLinks(args_playlist_url)
+        #Initalize VLC and media player
+        Instance = vlc.Instance()
+        player = Instance.media_player_new()
 
-    #Initalize VLC and media player
-    Instance = vlc.Instance()
-    player = Instance.media_player_new()
+        set_print = True
 
-    set_print = True
+        select_random_url_int = random.randrange(len(playlist_dict))
+        video = pafy.new(playlist_dict[select_random_url_int])
+        best = video.getbest()
+        playurl = best.url
 
-    select_random_url_int = random.randrange(len(playlist_dict))
-
-    video = pafy.new(playlist_dict[select_random_url_int][0])
-    best = video.getbest()
-    playurl = best.url
-
-    Media = Instance.media_new(playurl)
-    Media.get_mrl()
-    player.set_media(Media)
-    player.play()
+        Media = Instance.media_new(playurl)
+        Media.get_mrl()
+        player.set_media(Media)
+        player.play()
 
 
 
     if check_shutdown_file() is True:
 
         print("The program will end at approximately "+str(end_time))
-        while time_to_pause > 0:
 
-            # State.Ended
-            if set_print == True:
-                print("Now playing "+playlist_dict[select_random_url_int][1])
-                print("Composed By :"+video.author)
-                set_print = False
+        if args_player_off is True:
+            while time_to_pause > 0:                
+                # State.Ended
+                if set_print == True:
+                    print("Now playing "+playlist_dict[select_random_url_int][1])
+                    print("Composed By :"+video.author)
+                    set_print = False
 
 
-            if str(player.get_state()) == "State.Ended":
-                print("Switching to the next video!")
-                player.stop()
-                select_random_url_int = random.randrange(len(playlist_dict))
+                if str(player.get_state()) == "State.Ended":
+                    print("Switching to the next video!")
+                    player.stop()
+                    select_random_url_int = random.randrange(len(playlist_dict))
 
-                video = pafy.new(playlist_dict[select_random_url_int][0])
-                best = video.getbest()
-                playurl = best.url
-                Media = Instance.media_new(playurl)
-                Media.get_mrl()
-                player.set_media(Media)
-                player.play()
-                set_print = True
+                    video = pafy.new(playlist_dict[select_random_url_int][0])
+                    best = video.getbest()
+                    playurl = best.url
+                    Media = Instance.media_new(playurl)
+                    Media.get_mrl()
+                    player.set_media(Media)
+                    player.play()
+                    set_print = True
 
-            print("The time remaining is "+ str(time_to_pause)+ " seconds")
-            time_to_pause -= 1
-            sleep(1)
+                print("The time remaining is "+ str(time_to_pause)+ " seconds")
+                time_to_pause -= 1
+                sleep(1)
+
+        else:
+            while time_to_pause > 0:
+                print("The time remaining is "+ str(time_to_pause)+ " seconds without the player!")
+                time_to_pause -= 1
+                sleep(1)
 
         print("Turned off!")
-        exit()
         subprocess.run('C:\\Windows\\System32\\shutdown.exe -h')
-
     else:
         print("Unable to shutdown, non existing program!")
 
